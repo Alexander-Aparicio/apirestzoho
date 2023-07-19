d11b398bb95bbef306af9137dd62c8df670abffc
Alexander-Aparicio <alexdel2022@gmail.com>
2023-07-19 01:59:29 -0500

    base apirest

--- controllers/data.controllers.js
+++ controllers/data.controllers.js
@@ -1,4 +1,6 @@
 const response = require('express');
+const axios = require("axios");
+const config = require('../axios/config');
 
 const dataGet = (req, res = response) => {
   
@@ -10,9 +12,34 @@ const dataGet = (req, res = response) => {
 
 const dataPost = (req, res = response) => {
   const body = req.body
+  // const { id, phone_a, phone_b, call_status, call_cost, duration } = body 
+  const {Name, Email,Precio,Tipo_de_M_quina} = body
+
+  let data = JSON.stringify({
+    data: [
+      {
+        Name: Name,
+        Email: Email,
+        Precio: Precio,
+        Tipo_de_M_quina: Tipo_de_M_quina,
+      },
+    ],
+  });
+
+  config.data = data
+
+  axios
+    .request(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
   res.json({
     msj: 'POST DATA',
-    body: body.name
+    body
   })
 
 }
